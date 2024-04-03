102baa5ff8075fc3e70db4b0a1f3581ec697641d
E. Witten <witten.e@northeastern.edu>
2024-04-03 13:09:06 -0400

    Split raw dataset into train and set

--- scripts/make_ISTD_RawSR.sh
+++ scripts/make_ISTD_RawSR.sh
@@ -18,16 +18,13 @@ python copy_files.py \
     --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-train.txt
 
 python copy_files.py \
-    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb/all \
     --out_dir /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train \
     --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-train.txt
 
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/clean/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_C
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/mask/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_B
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/shadow/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_A
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/clean
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/mask
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/shadow
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/clean /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_C
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/mask /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_B
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/shadow /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/train/train_A
 
 # Copy test files
 python copy_files.py \
@@ -36,13 +33,10 @@ python copy_files.py \
     --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-test.txt
 
 python copy_files.py \
-    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb/all \
     --out_dir /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test \
     --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-test.txt
 
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/clean/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_C
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/mask/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_B
-mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/shadow/* /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_A
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/clean
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/mask
-rm -r /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/shadow
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/clean /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_C
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/mask /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_B
+mv /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/shadow /work/SuperResolutionData/ShadowRemovalData/ISTD_RawSR_Dataset/test/test_A
---
+++ scripts/split_RawSR.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=raw
+#SBATCH --time=1:00:00
+#SBATCH --partition=short
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=8
+#SBATCH --mem=50G
+#SBATCH --output=./out/split_RawSR.%j.out
+#SBATCH --error=./out/split_RawSR.%j.err
+echo "Setting up..."
+source raw_env/bin/activate
+
+# Copy training files
+python copy_files.py \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear \
+    --out_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear_split/train \
+    --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-train.txt
+
+python copy_files.py \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb \
+    --out_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb_split/train \
+    --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-train.txt
+
+# Copy test files
+python copy_files.py \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear \
+    --out_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear_split/test \
+    --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-test.txt
+
+python copy_files.py \
+    --in_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb \
+    --out_dir /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb_split/test \
+    --filenames_txt /home/witten.e/Shadow_Removal/raw-shadow-pipeline/files/ISTD_RawSR-test.txt
+
+# Re-organize
+mv /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear_split/all
+mv /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb_split/all
+mv /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear_split /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/linear
+mv /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb_split /work/SuperResolutionData/ShadowRemovalData/RawSR_Dataset_final/srgb
